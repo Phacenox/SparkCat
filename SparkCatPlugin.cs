d6532d229d64205c8a5fa432772df0b222faba68
Phacenox <phacenox@gmail.com>
2023-08-30 19:08:34 -0700

    Add project files.

---
+++ SparkCatPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoreSlugcats;
+using RWCustom;
+using UnityEngine;
+using Noise;
+using BepInEx;
+
+namespace SparkCat
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Plugin: BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "phace.sparkcat";
+        public const string PLUGIN_NAME = "Impulse";
+        public const string PLUGIN_VERSION = "0.0.1";
+    }
+}
